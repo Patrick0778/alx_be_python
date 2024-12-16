641c65345ddd4fd0ce304565e146f6f86bf3be97
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2024-12-16 23:27:35 +0300

    Initial commit

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -3,7 +3,6 @@ if weather == "sunny":
         print ("Wear a t-shirt and sunglasses.")
 elif weather == "rainy" :
         print("Don't forget your umbrella and a raincoat.")
-
 elif weather == "cold":
         print("Make sure to wear a warm coat and a scarf.")
 else :
