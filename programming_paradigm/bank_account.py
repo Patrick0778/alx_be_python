ece7bc8171463518ed5afe066e261eeffaf8d179
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-16 21:48:31 +0300

    added bank_account and main

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,16 @@
+class BankAccount:
+    def __init__(self, balance=0):
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        return self.balance
+
+    def withdraw(self, amount):
+        if amount > self.balance:
+            return 'Insufficient funds'
+        self.balance -= amount
+        return self.balance
+
+    def display_balance(self):
+        return self.balance
