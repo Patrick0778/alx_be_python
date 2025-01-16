5bfbca6854c9f1e1f91d741754f73c7878a42d9c
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-17 01:18:23 +0300

    updated calculator

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,27 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestSimpleCalculator(unittest.TestCase):
+    def setUp(self):
+        self.calculator = SimpleCalculator()
+
+    def test_add(self):
+        self.assertEqual(self.calculator.add(1, 2), 3)
+        self.assertEqual(self.calculator.add(-1, 1), 0)
+        self.assertEqual(self.calculator.add(0, 0), 0)
+
+    def test_subtract(self):
+        self.assertEqual(self.calculator.subtract(1, 2), -1)
+        self.assertEqual(self.calculator.subtract(-1, 1), -2)
+        self.assertEqual(self.calculator.subtract(0, 0), 0)
+
+    def test_multiply(self):
+        self.assertEqual(self.calculator.multiply(1, 2), 2)
+        self.assertEqual(self.calculator.multiply(-1, 1), -1)
+        self.assertEqual(self.calculator.multiply(0, 0), 0)
+
+    def test_divide(self):
+        self.assertEqual(self.calculator.divide(1, 2), 0.5)
+        self.assertEqual(self.calculator.divide(-1, 1), -1)
+        self.assertEqual(self.calculator.divide(0, 0), None)
+        self.assertEqual(self.calculator.divide(1, 0), None)
