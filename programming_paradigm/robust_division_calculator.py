d198c2d6965a71863f96ec5cb0c1baf73897fce7
Patrick0778 <160255522+Patrick0778@users.noreply.github.com>
2025-01-16 23:19:53 +0300

    updated robust_division

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -3,7 +3,7 @@ def safe_divide(numerator, denominator, default=None):
         num = float(numerator)
         den = float(denominator)
         if den == 0:
-            raise ZeroDivisionError("Cannot divide by zero.")
+            raise ZeroDivisionError("Error: Cannot divide by zero.")
         return num / den
     except ZeroDivisionError as e:
         print(f"Error: {e}")
